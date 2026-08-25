942b3c16db2b9adaf3fe8e3d6a3629b9fe9a0a8b
Isa Pompermaier <isadorapompermaier2009@gmail.com>
2026-08-25 10:45:34 -0300

    atualizei o cod do arduino pra so com o led e o buzzer

--- arduino/vigia_firmware/vigia_firmware.ino
+++ arduino/vigia_firmware/vigia_firmware.ino
@@ -1,57 +1,50 @@
-// para rodar no arduino uno
-// ponte H L298N (Controle do motor)
-const int PIN_IN1 = 4;     // in1 da Ponte H
-const int PIN_IN2 = 5;     // in2 da Ponte H
+// parte que deve ser rodada no arduino IDE em c++
 
-// alertas na Protoboard
-const int PIN_BUZZER = 10; 
-const int PIN_LED1 = 12;  
-const int PIN_LED2 = 13;  
+// definicao dos pinos
+const int PIN_BUZZER = 10; //sinal do buzzer
+const int PIN_LED1   = 12; //led de atencao
+const int PIN_LED2   = 13; // led de fadiga critica
 
 void setup() {
+  // tempo de comunicao com o computador
   Serial.begin(9600);
 
-  pinMode(PIN_IN1, OUTPUT);
-  pinMode(PIN_IN2, OUTPUT);
+  // configura od pinos de saida
   pinMode(PIN_BUZZER, OUTPUT);
   pinMode(PIN_LED1, OUTPUT);
   pinMode(PIN_LED2, OUTPUT);
 
-  // estado inicial: motor LIGADO, LEDs e Buzzer DESLIGADOS
-  digitalWrite(PIN_IN1, HIGH);
-  digitalWrite(PIN_IN2, LOW);
+  //comeca com tudo desligado
+  noTone(PIN_BUZZER);
+  digitalWrite(PIN_BUZZER, LOW);
   digitalWrite(PIN_LED1, LOW);
   digitalWrite(PIN_LED2, LOW);
-  digitalWrite(PIN_BUZZER, LOW);
 }
 
 void loop() {
+  //verifica se algum comando foi enviado
   if (Serial.available() > 0) {
     char comando = Serial.read();
 
     if (comando == '0') {
-      // estado 0: normal = liga motor, desliga os avisos
-      digitalWrite(PIN_IN1, HIGH);
-      digitalWrite(PIN_IN2, LOW);
+      // 0: normal (olhos abertos) tudo desligado
+      noTone(PIN_BUZZER);
+      digitalWrite(PIN_BUZZER, LOW);
       digitalWrite(PIN_LED1, LOW);
       digitalWrite(PIN_LED2, LOW);
-      digitalWrite(PIN_BUZZER, LOW);
     } 
     else if (comando == '1') {
-      // estado 1: ATENÇÃO = mantem o motor, liga LED 1
-      digitalWrite(PIN_IN1, HIGH);
-      digitalWrite(PIN_IN2, LOW);
+      // 1: atencao: piscada longa liga apenas um led
+      noTone(PIN_BUZZER);
+      digitalWrite(PIN_BUZZER, LOW);
       digitalWrite(PIN_LED1, HIGH);
       digitalWrite(PIN_LED2, LOW);
-      digitalWrite(PIN_BUZZER, LOW);
     } 
     else if (comando == '2') {
-      // ESTADO 2: FADIGA CRÍTICA -> PARA ROTOR, LIGA BUZZER E LEDS
-      digitalWrite(PIN_IN1, LOW);  // DESLIGA MOTOR
-      digitalWrite(PIN_IN2, LOW);
-      digitalWrite(PIN_LED1, HIGH); // LIGA LED 1
-      digitalWrite(PIN_LED2, HIGH); // LIGA LED 2
-      digitalWrite(PIN_BUZZER, HIGH); // LIGA BUZZER
+      // 2: "dormiu" : toca o buzzer e acende os dois leds
+      tone(PIN_BUZZER, 1200);       // emite tom de 1200 Hz
+      digitalWrite(PIN_LED1, HIGH);
+      digitalWrite(PIN_LED2, HIGH);
     }
   }
 }
